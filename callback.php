53d7ff892c165d64777d35168d69f1c3b3f01ce3
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-06-29 23:02:23 +0700

    add callback page

---
+++ callback.php
@@ -0,0 +1,12 @@
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LIFF Starter</title>
+</head>
+
+<body>
+    <h1>Welcome to callback page</h1>
+    <script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
+    <script>
+      liff.init()
+    </script>
+</body>
