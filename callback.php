47dc5bbe10989ebfd3f6a3b04204527c30260e15
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-07-08 00:25:51 +0700

    show line data

--- callback.php
+++ callback.php
@@ -6,11 +6,31 @@
 <body>
     <h1>Welcome to callback page</h1>
     <a href="page2.php">go to page 2</a><br/>
+    <div id="profilepicturediv">
+    </div><br/>
+    <div id="useridprofilefield"></div><br/>
+    <div id="displaynamefield"></div><br/>
+    <div id="statusmessagefield"></div><br/>
     <script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
     <script>
-        alert()
         liff.init();
-        // liff.getProfile()
-        document.write("<p>" + JSON.stringify(liff.getProfile()) + "</p>");
+
+        liff.getProfile().then(function (profile) {
+            document.getElementById('useridprofilefield').textContent = profile.userId;
+            document.getElementById('displaynamefield').textContent = profile.displayName;
+
+            var profilePictureDiv = document.getElementById('profilepicturediv');
+            if (profilePictureDiv.firstElementChild) {
+                profilePictureDiv.removeChild(profilePictureDiv.firstElementChild);
+            }
+            var img = document.createElement('img');
+            img.src = profile.pictureUrl;
+            img.alt = "Profile Picture";
+            profilePictureDiv.appendChild(img);
+
+            document.getElementById('statusmessagefield').textContent = profile.statusMessage;
+        }).catch(function (error) {
+            window.alert("Error getting profile: " + error);
+        });
     </script>
 </body>
