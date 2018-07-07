104cae5c455d8ebd10b586a4f80366e4c9fc0467
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-07-08 00:11:58 +0700

    get profile data

--- callback.php
+++ callback.php
@@ -5,9 +5,18 @@
 
 <body>
     <h1>Welcome to callback page</h1>
-    <a href="page2.php">go to page 2</a>
-    <!-- <script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script> -->
+    <a href="page2.php">go to page 2</a><br/>
+    <script src="https://d.line-scdn.net/liff/1.0/sdk.js"></script>
     <script>
-    //   liff.init()
+      liff.init()
+      liff.getProfile()
+        .then(profile => {
+            var name = profile.displayName;
+            document.write("<p>" + name + "</p>");
+            document.write("<p>" + profile + "</p>");
+        })
+        .catch((err) => {
+            console.log('error', err);
+        });
     </script>
 </body>
